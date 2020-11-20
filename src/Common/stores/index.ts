b063e4007eeec72364e140f840d6c982931cc2e4
hafeezulkareem <hafeezulkareem20@gmail.com>
2020-11-20 10:19:38 +0530

    Remove stays parameter for Windbnb store

--- src/Common/stores/index.ts
+++ src/Common/stores/index.ts
@@ -1,6 +1,5 @@
-import stays from '../../data/stays.json'
 import WindbnbStore from '../../Windbnb/stores/WindbnbStore/WindbnbStore'
 
-const windbnbStore = new WindbnbStore(stays)
+const windbnbStore = new WindbnbStore()
 
 export default { windbnbStore }
--- src/Windbnb/stores/WindbnbStore/WindbnbStore.ts
+++ src/Windbnb/stores/WindbnbStore/WindbnbStore.ts
@@ -1,11 +1,13 @@
 import { observable } from 'mobx'
 
+import stays from '../../../data/stays.json'
+
 import { StayType } from '../types'
 
 class WindbnbStore {
    @observable stays: Array<StayType>
 
-   constructor(stays: Array<StayType>) {
+   constructor() {
       this.stays = stays
    }
 }
